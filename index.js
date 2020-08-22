b243550dd96563075b2e36cdf60cc126ffc815d8
aliking66 <30318556+aliking66@users.noreply.github.com>
2020-08-22 14:32:24 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function Housekeeper(name,age,experience,interests){
+
+    this.name=name;
+    this.age=age;
+    this.experience=experience;
+    this.interests=interests;
+    }
+
+
+    var housekeeper1=new Housekeeper("ali",19,2,"lauphing");
+    var housekeeper2=new Housekeeper("amir",21,3,"working");
